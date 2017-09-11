c68fa60c87b51df58a30e9dccf617a26af987208
deadlybutter <thedeadlybutter@gmail.com>
2017-09-11 16:00:49 -0400

    Adding validation tests, fixing discovered bugs

---
+++ test/validator.js
@@ -0,0 +1,54 @@
+const assert = require('chai').assert;
+const isValid = require('../lib/validator');
+
+describe('Validates data', function() {
+  it ('should not validate an object with a bad key', function() {
+    const invalidObject = {
+      test: 'this will fail',
+      event: {},
+      meta: {},
+      user: {},
+      page: {},
+      data: {},
+    };
+
+    assert.isFalse(isValid(invalidObject));
+  });
+
+  it ('should not validate an object missing keys', function() {
+    const invalidObject = {
+      event: {},
+      meta: {},
+      user: {},
+      page: {},
+    };
+
+    assert.isFalse(isValid(invalidObject));
+  });
+
+  it ('should not validate an object over 4000 bytes', function() {
+    const largeObject = new Array(4000).join('0').split('');
+
+    const invalidObject = {
+      event: {},
+      meta: {},
+      user: {},
+      page: {},
+      data: largeObject,
+    };
+
+    assert.isFalse(isValid(invalidObject));
+  });
+
+  it ('should validate an object with proper keys & size limit', function() {
+    const validObject = {
+      event: {},
+      meta: {},
+      user: {},
+      page: {},
+      data: {},
+    };
+
+    assert.isTrue(isValid(validObject));
+  });
+});
