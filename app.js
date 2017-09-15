5f9f5647b80d84cea043e4e2697207aecd5dc1b2
deadlybutter <thedeadlybutter@gmail.com>
2017-09-15 16:32:53 -0400

    Removing spec and allowing cross origin

--- app.js
+++ app.js
@@ -1,40 +1,33 @@
 const keypunch = require('keypunch');
 const sticky = require('sticky-session');
-const cluster = require('cluster');
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
 const stathat = require('./util/stathat');
 
-const totalCPUs = require('os').cpus().length;
+const app = require('express')();
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+keypunch.info('Starting up...');
+
+const { distribute } = require('./lib/distributor');
+
+io.on('connection', (socket) => {
+  keypunch.log(`Socket id ${socket.id} connected`);
+  stathat.count('client connection');
+
+  socket.on('analytics', (data) => {
+    keypunch.log(`Socket ${socket.id} sent an event`);
+    stathat.count('event delivered');
+    distribute(typeof data === 'string' ? JSON.parse(data) : data);
+  });
+});
+
 const port = process.env.PORT;
-
-if (cluster.isMaster) {
-  keypunch.addHeaderFunction(() => '[Leader]');
-  keypunch.info(`Spinning up ${totalCPUs} processes...'`);
-
-  for (let workerIndex = 0; workerIndex < totalCPUs; workerIndex++) {
-    cluster.fork();
-  }
-} else if (cluster.isWorker){
-  keypunch.addHeaderFunction(() => `[Worker #${cluster.worker.id}]`);
-  keypunch.info('Starting up...');
-
-  const { distribute } = require('./lib/distributor');
-
-  io.on('connection', (socket) => {
-    keypunch.log(`Socket id ${socket.id} connected`);
-    stathat.count('client connection');
-
-    socket.on('analytics', (data) => {
-      keypunch.log(`Socket ${socket.id} sent an event`);
-      stathat.count('event delivered');
-      distribute(typeof data === 'string' ? JSON.parse(data) : data);
-    });
-  });
-
-  const port = process.env.PORT;
-  http.listen(port, () => {
-    keypunch.info(`Listening on port ${port}`);
-  });
-}
+http.listen(port, () => {
+  keypunch.info(`Listening on port ${port}`);
+});
