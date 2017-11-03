89da43426f319321938b226429630dfa75f81c11
deadlybutter <thedeadlybutter@gmail.com>
2017-11-03 11:44:59 -0400

    Removing old stathat lib

--- util/stathat.js
+++ util/stathat.js
@@ -1,6 +1,5 @@
 const stathat = require('stathat');
 const STATHAT_KEY = process.env.STATHAT_KEY;
-const stats = STATHAT_KEY ? new tophat(STATHAT_KEY) : null;
 
 module.exports.count = (name) => {
   if (STATHAT_KEY) {
