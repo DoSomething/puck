6046f290f29676270d835bdbc1a8d676f3a48339
Mendel Blesofsky <mendelblesofsky@gmail.com>
2019-02-05 17:36:35 -0500

    Fix function call

--- scripts/keen-event-backfill.js
+++ scripts/keen-event-backfill.js
@@ -93,7 +93,7 @@ async function run(client) {
     const collections = response.map(collection => collection.name);
 
     // Process the collections.
-    await processCollections(collections);
+    await processKeenCollections(collections);
 
     // Close the connection to the Puck database.
     db.close();
